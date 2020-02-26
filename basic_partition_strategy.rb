b020b0c03c05e379e25efb4a4108fedee67967d7
Andy <andy@Andys-MacBook-Pro.local>
2020-02-26 14:44:22 -0500

    proc

--- basic_partition_strategy.rb
+++ basic_partition_strategy.rb
@@ -1,10 +1,14 @@
 class BasicPartitionStrategy
 
+  def initialize(compare)
+    @compare = compare
+  end
+
   def partition(data, first, last)
     pivot = data[first]
     right_first = first+1
     (right_first..last).each do |i|
-      if(data[i] < pivot)
+      if(@compare.call(data[i], pivot))
         swap(data, i, right_first)
         right_first+=1
       end
@@ -15,6 +19,8 @@ class BasicPartitionStrategy
     return mid
   end
   
+  private
+
   def swap(data, a, b)
     if(a != b)
       x = data[a]
--- main.rb
+++ main.rb
@@ -1,9 +1,8 @@
-
-
 require './quicksort'
 require './basic_partition_strategy'
 
-partition_strategy = BasicPartitionStrategy.new
+compare = Proc.new{|item, pivot| item < pivot }
+partition_strategy = BasicPartitionStrategy.new(compare)
 quicksort = Quicksort.new(partition_strategy)
 data = [5,2,7,4,1,6,3]
 quicksort.sort(data, 0, data.length-1)
