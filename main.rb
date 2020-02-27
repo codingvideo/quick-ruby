41e1d4527aae5150fad0c5893f2826fcc37d3d2e
Andy <andy@Andys-MacBook-Pro.local>
2020-02-26 19:26:57 -0500

    input prompt

--- main.rb
+++ main.rb
@@ -1,17 +1,15 @@
 require './quicksort'
-require './basic_partition_strategy'
+# require './basic_partition_strategy'
 require './efficient_partition_strategy'
+
+print "Please enter numbers: "
+input = gets
+data = []
+input.split.each do |item|
+  data << item.to_i
+end
 
 efficient_strategy = EfficientPartitionStrategy.new
 quicksort = Quicksort.new(efficient_strategy)
-data1 = [5,2,7,4,1,6,3]
-quicksort.sort(data1, 0, data1.length-1)
-puts data1.join(', ')
-
-# change strategy but use the same quicksort object
-compare = Proc.new{|item, pivot| item < pivot }
-basic_strategy = BasicPartitionStrategy.new(compare)
-quicksort.strategy = basic_strategy
-data2 = [5,2,7,4,1]
-quicksort.sort(data2, 0, data2.length-1)
-puts data2.join(', ')
+quicksort.sort(data, 0, data.length-1)
+puts data.join(', ')
